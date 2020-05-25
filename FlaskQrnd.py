5a61dd17c4e8db789ebb8dcfe8e85a8daec091b8
Ubuntu <ubuntu@ip-172-31-13-171.ap-southeast-1.compute.internal>
2020-05-25 12:14:35 +0000

    Updated to get it working.

--- FlaskQrnd.py
+++ FlaskQrnd.py
@@ -8,9 +8,11 @@ from qiskit.tools.monitor import job_monitor
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit, execute,IBMQ
 import json 
 
+IBMQ.enable_account('983b7e03809ecbd0c79e68f8cd52cdaff7426519e8c72a6dfd47d2297415d60b2850c213de650c4b79bc835d7a22939683a472b184d851344833965a426f7c5f')
+
 def runQC():
-    #IBMQ.enable_account('ENTER API TOKEN HERE')
-    IBMQ.load_account()
+    #IBMQ.enable_account('983b7e03809ecbd0c79e68f8cd52cdaff7426519e8c72a6dfd47d2297415d60b2850c213de650c4b79bc835d7a22939683a472b184d851344833965a426f7c5f')
+    #IBMQ.load_account()
     provider = IBMQ.get_provider(hub='ibm-q')
     nqubits = 5
     q = QuantumRegister(nqubits,'q')
@@ -18,24 +20,24 @@ def runQC():
     circuit = QuantumCircuit(q,c)
     circuit.h(q) # Applies hadamard gate to all qubits
     circuit.measure(q,c) # Measures all qubits 
-    '''backend = least_busy(provider.backends(filters=lambda x: x.configuration().n_qubits >= nqubits 
+    backend = least_busy(provider.backends(filters=lambda x: x.configuration().n_qubits >= nqubits 
                                            and not x.configuration().simulator 
-                                           and x.status().operational==True))'''
+                                           and x.status().operational==True))
 
-    backend = provider.get_backend('ibmq_qasm_simulator')
+    #backend = provider.get_backend('ibmq_qasm_simulator')
     print("least busy backend: ", backend)
 
     job = execute(circuit, backend, shots=1)
-    job_monitor(job)
+    #job_monitor(job)
     result = job.result()
-    counts = result.get_counts(circuit)
+    counts = str(result.get_counts(circuit))
 
-    return counts
+    return counts + " from " + str(backend)
 
 
 # In[ ]:
 
-print(str(runQC()))
+#print(str(runQC()))
 
 
 # In[ ]:
